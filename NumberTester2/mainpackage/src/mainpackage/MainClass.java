5d8f8dfff714e61cdc5810d1a6d80dcd1af303c7
Oliver Droop <oliverdroop@hotmail.co.uk>
2020-02-16 00:40:34 +0000

    Add slf4j and log4j to project

--- NumberTester2/mainpackage/src/mainpackage/MainClass.java
+++ NumberTester2/mainpackage/src/mainpackage/MainClass.java
@@ -1,9 +1,11 @@
 package mainpackage;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class MainClass {
     public static void main(String[] args){
-        List<Integer> primes = new PrimeFinder().findPrimes(1000000);
+        //List<Integer> primes = new PrimeFinder().findPrimes(1000000);
+        Visualizer v = new Visualizer();
+        v.drawSpiral(new ArrayList<Integer>());
     }
 }
--- NumberTester2/mainpackage/src/mainpackage/Visualizer.java
+++ NumberTester2/mainpackage/src/mainpackage/Visualizer.java
@@ -1,26 +1,38 @@
 package mainpackage;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.Logger;
 
 public class Visualizer {
-    private static final Logger LOGGER = LoggerFactory.Logger.getLogger(Visualizer.class);
+    private static final Logger LOGGER = Logger.getLogger(Visualizer.class);
     String path = "D:\\Documents and Settings\\Oliver\\My Documents\\My Pictures\\Computer Art\\Number Images\\";
     public void drawSpiral(List<Integer> numberList){
-        
+        BufferedImage img = new BufferedImage(20, 20, BufferedImage.TYPE_INT_RGB);
+        Graphics g = img.getGraphics();
+        g.drawLine(0, 0, 19, 19);
+        savePicture(img, path + "myImage.bmp");
+        LOGGER.info("Saved picture");
     }
     
     public void savePicture(BufferedImage img, String path){
         try{
-            
+            FileWriter fw = new FileWriter(path);
+            File outputFile = new File(path);
+            ImageIO.write(img, "bmp", outputFile);
+                
         }
         catch(IOException ioe){
-            
+            LOGGER.info(ioe.getMessage());
         }
     }
 }
