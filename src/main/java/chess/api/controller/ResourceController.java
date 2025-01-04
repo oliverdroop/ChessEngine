a6f209c077ff0d02d8bbf48d53f87556f04faff3
oliverdroop <oliverdroop@hotmail.co.uk>
2025-01-04 18:05:00 +0000

    Use ClassPathResource to load resources

--- src/main/java/chess/api/controller/ResourceController.java
+++ src/main/java/chess/api/controller/ResourceController.java
@@ -2,16 +2,14 @@ package chess.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 
 import static java.lang.String.format;
@@ -22,9 +20,9 @@ public class ResourceController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ResourceController.class);
 
     @GetMapping("/{path}")
-    public ResponseEntity<byte[]> getResource(@PathVariable String path) throws FileNotFoundException {
-        final File file = ResourceUtils.getFile(format("classpath:%s", path));
-        try(final FileInputStream fileInputStream = new FileInputStream(file)) {
+    public ResponseEntity<byte[]> getResource(@PathVariable String path) {
+        final ClassPathResource resource = new ClassPathResource(path);
+        try(final FileInputStream fileInputStream = new FileInputStream(resource.getFile())) {
             return ResponseEntity.ok(fileInputStream.readAllBytes());
         } catch (Throwable e) {
             LOGGER.error("Unable to return {}", path, e);
