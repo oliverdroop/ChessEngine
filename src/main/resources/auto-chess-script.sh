9939246db28970dd879948d14ce73d4518854c71
oliverdroop <oliverdroop@hotmail.co.uk>
2025-11-23 21:22:34 +0000

    Add move history to auto-chess-script.sh

--- src/main/resources/auto-chess-script.sh
+++ src/main/resources/auto-chess-script.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 CURL='/usr/bin/curl'
-#URL=https://g1zhh0aw57.execute-api.eu-west-2.amazonaws.com/default/chess-application-1_2_4-lambda
-URL=https://odroop.co.uk:8081/chess
+URL=https://g1zhh0aw57.execute-api.eu-west-2.amazonaws.com/default/chess-application-1_2_4-lambda
+#URL=https://odroop.co.uk:8081/chess
 FEN='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 FEN_REGEX='(([bknpqrBKNPQR1-8]{1,8})/){7}[bknpqrBKNPQR1-8]{1,8} [bw] (K?Q?k?q?|-) (([a-h][36])|-) [0-9]{1,3} [0-9]{1,4}'
+GAME_END_REGEX='(DRAW|STALEMATE|WHITE_VICTORY|BLACK_VICTORY){1}'
+ALGEBRAIC_NOTATION_REGEX='[a-h][1-8]x?[a-h][1-8][qrbn]?'
+MOVE_HISTORY=()
+MOVE_HISTORY_STRING=""
 PLAYING=true
 
 generate_post_data()
 {
   cat <<EOF
-{"depth":4,"fen":"$FEN"}
+{"depth":4,"fen":"$FEN","moveHistory":[${MOVE_HISTORY_STRING}]}
 EOF
 }
 
@@ -22,6 +26,18 @@ do
 	if [[ "$RESULT" =~ $FEN_REGEX ]]; then
 		echo "${BASH_REMATCH[0]}"
 		FEN="${BASH_REMATCH[0]}"
+
+		if [[ "$RESULT" =~ $ALGEBRAIC_NOTATION_REGEX ]]; then
+			MOVE_HISTORY+=("\"${BASH_REMATCH[0]}\"")
+			printf -v joined '%s,' "${MOVE_HISTORY[@]}"
+			#echo "${joined%,}"
+			MOVE_HISTORY_STRING="${joined%,}"
+		fi
+
+		if [[ "$RESULT" =~ $GAME_END_REGEX ]]; then
+			PLAYING=false
+			echo $RESULT
+		fi
 	else
 		PLAYING=false
 		echo $RESULT
